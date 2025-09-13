b9f1148e6cd3189726a06fbabc2050e4d2bab398
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2025-09-13 22:11:23 +0200

    Update source path and history config

--- source/50_history.sh
+++ source/50_history.sh
@@ -1,18 +1,12 @@
-# History settings
+setopt hist_ignore_all_dups inc_append_history
+setopt share_history
+HISTFILE=~/.zsh_history
+HISTORY_IGNORE='(l|ls|ll|cd|cd ..|z|pwd|exit|date|history|tmux|tmux attach)'
+HISTSIZE=290000
+SAVEHIST=$HISTSIZE
 
-# Allow use to re-edit a faild history substitution.
-shopt -s histreedit
-# History expansions will be verified before execution.
-shopt -s histverify
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_USE_ASYNC=1
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 
-# Entries beginning with space aren't added into history, and duplicate
-# entries will be erased (leaving the most recent entry).
-export HISTCONTROL="ignorespace:erasedups"
-# Give history timestamps.
-export HISTTIMEFORMAT="[%F %T] "
-# Lots o' history.
-export HISTSIZE=10000
-export HISTFILESIZE=10000
-
-# Easily re-execute the last history command.
-alias r="fc -s"
+export ERL_AFLAGS="-kernel shell_history enabled"
