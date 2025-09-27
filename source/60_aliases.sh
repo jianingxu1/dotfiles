4368bad0291d2c7eea2a3f286e007e768436d1b0
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2025-09-27 17:36:59 +0200

    Add arc and git aliases

--- source/60_aliases.sh
+++ source/60_aliases.sh
@@ -4,3 +4,27 @@ alias path='echo $PATH | tr -s ":" "\n"'
 alias sconfig="source ~/.zshrc"
 alias vconfig="vim ~/.zshrc"
 alias svenv="source .venv/bin/activate"
+
+alias af="arc flow"
+alias ad="arc diff"
+alias ap="arc patch"
+
+alias gbr="git branch"
+alias gco='git checkout'
+alias gsw="git switch"
+
+alias gad="git add"
+alias gco="git commit --verbose"
+
+alias gcam="git cam"
+alias gpu="git pull"
+
+alias grt="git restore"
+alias gfix="git commit --amend --verbose"
+alias gafix="git commit -a --amend --verbose"
+
+alias grei="git rebase -i"
+alias greiall="git rebase -i --root"
+alias gcont="git rebase --continue"
+
+alias gdiff="git diff"
