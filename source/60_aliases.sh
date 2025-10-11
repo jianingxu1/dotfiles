e9ad032ec923c69f7b049907165bed352cdf8542
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2025-10-11 14:45:06 +0200

    Update git aliases and uninstall ghostscript

--- source/60_aliases.sh
+++ source/60_aliases.sh
@@ -5,26 +5,32 @@ alias sconfig="source ~/.zshrc"
 alias vconfig="vim ~/.zshrc"
 alias svenv="source .venv/bin/activate"
 
+# Arc
 alias af="arc flow"
 alias ad="arc diff"
 alias ap="arc patch"
 
+# Information
+alias gs="git status --short --branch"
+alias glog="git log --pretty=format:'%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]' --decorate --date=short"
+
+# Navigation
 alias gbr="git branch"
 alias gco='git checkout'
 alias gsw="git switch"
 
+# Commit
 alias gad="git add"
 alias gco="git commit --verbose"
+alias gcm="git commit --message"
+alias gcam="git commit --all --message"
 
-alias gcam="git cam"
-alias gpu="git pull"
-
+# Fixing
 alias grt="git restore"
 alias gfix="git commit --amend --verbose"
-alias gafix="git commit -a --amend --verbose"
-
 alias grei="git rebase -i"
 alias greiall="git rebase -i --root"
 alias gcont="git rebase --continue"
 
-alias gdiff="git diff"
+alias gd="git diff"
+alias gpu="git pull"
