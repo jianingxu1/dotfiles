4079e612ba423167bab31610685dccd3d445ea05
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2025-10-18 00:27:30 +0200

    Change p10k to install only on osx

--- source/30_p10k.sh
+++ source/30_p10k.sh
@@ -1,3 +1,6 @@
+# OSX-only stuff. Abort if not OSX.
+is_osx || return 1
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
