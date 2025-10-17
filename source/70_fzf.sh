6eec01d61cfaa5f5628a3ebb95f5d88f6435432b
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2025-10-18 00:59:20 +0200

    Fix reverse search with fzf setup in devpod

--- source/70_fzf.sh
+++ source/70_fzf.sh
@@ -1,1 +1,12 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if [[ ! -z "$DEVPOD_NAME" ]]; then
+    # Install latest version on devpod
+    if [[ ! -f "$HOME/.fzf.zsh" ]]; then
+        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+        echo "y y n " | tr ' ' '\n' | ~/.fzf/install
+    fi
+    export FZF_PATH="$HOME/.fzf"
+    export PATH="$HOME/.fzf/bin:$PATH"
+    source "$HOME/.fzf.zsh"
+fi
+
+source <(fzf --zsh)
