d92999357c399437a6c6b7d2194e31364237b59d
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2025-10-18 12:22:53 +0200

    Config ctrl+r and ctrl+t for fzf

--- source/70_fzf.sh
+++ source/70_fzf.sh
@@ -1,3 +1,15 @@
+# CTRL-S to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --bind 'ctrl-s:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-S to copy command into clipboard'"
+
+# Preview file content using bat (https://github.com/sharkdp/bat)
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
 if [[ ! -z "$DEVPOD_NAME" ]]; then
     # Install latest version on devpod
     if [[ ! -f "$HOME/.fzf.zsh" ]]; then
