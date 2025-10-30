722ae9d8d250d8a8ad4656e84d8002f125d2d4ed
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2025-10-30 22:00:44 +0100

    Use vim as default editor

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -3,13 +3,6 @@
 export VISUAL=vim
 export EDITOR="$VISUAL"
 
-# VS Code
-if [[ "$(which code)" ]]; then
-  EDITOR="code --wait"
-  VISUAL="code --wait --new-window"
-  unset GIT_EDITOR
-fi
-
 function q() {
   if [[ -t 0 ]]; then
     $VISUAL "$@"
