54a6f2782b0f19d26bb9a45e3d6da1952a3ba6b7
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2025-10-30 21:49:21 +0100

    Run oh my zsh and p10k setup on Linux

--- source/30_p10k.sh
+++ source/30_p10k.sh
@@ -1,6 +1,3 @@
-# OSX-only stuff. Abort if not OSX.
-is_osx || return 1
-
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
