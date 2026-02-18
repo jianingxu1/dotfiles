1566f4d1b64feb9630834cf3236e113d43704a2a
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2026-02-18 22:25:35 +0000

    Increase start up time
    - p10k instant prompt
    - Use zgenom for plugins and remove OMZ manual installation

--- source/20_plugins.sh
+++ source/20_plugins.sh
@@ -1,25 +1,24 @@
-if [[ -n "$ZSH_ZGENOM_PLUGINS_LOADED" ]]; then
-  return
-fi
-ZSH_ZGENOM_PLUGINS_LOADED=1
+# load zgenom
+source "${HOME}/.zgenom/zgenom.zsh"
 
-ZGENOM_DIR="${ZGENOM_DIR:-$HOME/.zgenom}"
-ZGENOM_PATH="$ZGENOM_DIR/zgenom.zsh"
-
-if [[ ! -r "$ZGENOM_PATH" ]]; then
-  return
-fi
-
-source "$ZGENOM_PATH"
-
+# if the init script doesn't exist
 if ! zgenom saved; then
-  zgenom ohmyzsh
-  zgenom load zsh-users/zsh-autosuggestions
-  zgenom load zsh-users/zsh-syntax-highlighting
-  zgenom load djui/alias-tips
-  zgenom load zsh-users/zsh-completions
-  zgenom load unixorn/fzf-zsh-plugin
-  zgenom load chitoku-k/fzf-zsh-completions
-  zgenom load toku-sa-n/zsh-dot-up
+  zgenom load p10k-instant-prompt # Speeds up startup time by a lot
+
+  zgenom load zsh-users/zsh-completions            # Extra completions for docker, npm, and more
+  zgenom load zsh-users/zsh-autosuggestions        # Suggests commands from history as you type; accept with right-arrow
+  zgenom load zsh-users/zsh-syntax-highlighting   # Colors commands green (valid) or red (error) as you type (load last)
+  zgenom load chitoku-k/fzf-zsh-completions  # Fuzzy-find and pick from the completion list
+  zgenom load unixorn/fzf-zsh-plugin          # fzf keybindings and shell integration (**<Tab>, etc.)
+  zgenom load aloxaf/fzf-tab                  # Tab completion via fzf instead of the default menu
+  zgenom load djui/alias-tips                      # Hints when an alias exists for the command you just ran
+
+  zgenom load ajeetdsouza/zoxide  # Smarter cd (z <partial path> jumps to freq-used dirs)
+  zgenom load toku-sa-n/zsh-dot-up  # dot-up command to update dotfiles from repo
+
+  zgenom load romkatv/powerlevel10k powerlevel10k  # Configurable prompt theme (path, git, time)
+
+  zgenom load unixorn/autoupdate-zgenom
+
   zgenom save
 fi
