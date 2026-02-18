47c8cec3cfe0f232dc5258c3d0e2d3ae9dbec18c
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2026-02-18 21:10:26 +0000

    top tracking plug.vim; install via init/.vimrc instead

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,7 +1,16 @@
 # Backups, swaps and undos are stored here.
 mkdir -p $DOTFILES/caches/vim
 
+# Ensure vim-plug is present (we don't track plug.vim; .vimrc also installs it on first open).
+PLUG_VIM="${DOTFILES}/link/.vim/autoload/plug.vim"
+if [[ ! -f "$PLUG_VIM" ]] && [[ "$(type -P curl)" ]]; then
+  mkdir -p "$(dirname "$PLUG_VIM")"
+  curl -fLo "$PLUG_VIM" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
   vim +PlugUpgrade +PlugUpdate +qall
 fi
+
